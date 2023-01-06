98d26934fe8843007195680c7db159bbdc1f7d7d
MelysaNguyen <mnguyen181@my.bcit.ca>
2023-01-06 12:10:13 -0800

    add map and waterfront marker

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Map from '../components/Map'
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        test
+        <Map />
       </main>
 
       <footer className={styles.footer}>
